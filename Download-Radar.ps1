1837f12f8c213f4e8467dd4366938a341ef97d5e
Joshua Porrata <Joshua.porrata@gmail.com>
2017-09-10 20:54:43 -0400

    fixed to work with 9-11 GMT scans

--- Download-Radar.ps1
+++ Download-Radar.ps1
@@ -1,14 +1,10 @@
 [string]$basePath = "http://thredds.ucar.edu/thredds/fileServer/";
-$keepalive = $true
-while ($keepalive) {
-    $catalog = [xml] (Get-Content -Path ./newcatalog.xml);
-    Invoke-WebRequest -Uri "http://thredds.ucar.edu/thredds/catalog/nexrad/level3/DTA/TBW/20170910/catalog.xml" -OutFile "newcatalog.xml";
-    $catalog = [xml] (Get-Content -Path ./newcatalog.xml);
-    $nidPath = $catalog.catalog.dataset.dataset.urlPath[0];
-    $dPath = ($basePath + $nidPath);
-    $dPath;
-    $outpath = ("./nids/" + $catalog.catalog.dataset.dataset.name[0])
-    Invoke-WebRequest -uri $dpath -OutFile $outpath -ErrorAction SilentlyContinue; 
-    Start-Sleep -Seconds 360;
-}
+$catalog = [xml] (Get-Content -Path ./newcatalog.xml);
+Invoke-WebRequest -Uri "http://thredds.ucar.edu/thredds/catalog/nexrad/level3/DTA/TBW/20170911/catalog.xml" -OutFile "newcatalog.xml";
+$catalog = [xml] (Get-Content -Path ./newcatalog.xml);
+$nidPath = $catalog.catalog.dataset.dataset.urlPath[0];
+$dPath = ($basePath + $nidPath);
+$dPath;
+$outpath = ("./nids/" + $catalog.catalog.dataset.dataset.name[0])
+Invoke-WebRequest -uri $dpath -OutFile $outpath;
 

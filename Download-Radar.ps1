e66fbe03a497399602df4ee8e7ddccd15d27b84c
Joshua Porrata <Joshua.porrata@gmail.com>
2017-09-10 20:58:54 -0400

    Added Keep alive, will pol every 6 minutes

--- Download-Radar.ps1
+++ Download-Radar.ps1
@@ -1,10 +1,12 @@
-[string]$basePath = "http://thredds.ucar.edu/thredds/fileServer/";
-$catalog = [xml] (Get-Content -Path ./newcatalog.xml);
-Invoke-WebRequest -Uri "http://thredds.ucar.edu/thredds/catalog/nexrad/level3/DTA/TBW/20170911/catalog.xml" -OutFile "newcatalog.xml";
-$catalog = [xml] (Get-Content -Path ./newcatalog.xml);
-$nidPath = $catalog.catalog.dataset.dataset.urlPath[0];
-$dPath = ($basePath + $nidPath);
-$dPath;
-$outpath = ("./nids/" + $catalog.catalog.dataset.dataset.name[0])
-Invoke-WebRequest -uri $dpath -OutFile $outpath;
-
+$keepalive = $true
+while ($keepalive) {
+    [string]$basePath = "http://thredds.ucar.edu/thredds/fileServer/";
+    $catalog = [xml] (Get-Content -Path ./newcatalog.xml);
+    Invoke-WebRequest -Uri "http://thredds.ucar.edu/thredds/catalog/nexrad/level3/DTA/TBW/20170911/catalog.xml" -OutFile "newcatalog.xml";
+    $nidPath = $catalog.catalog.dataset.dataset.urlPath[0];
+    $dPath = ($basePath + $nidPath);
+    $dPath;
+    $outpath = ("./nids/" + $catalog.catalog.dataset.dataset.name[0])
+    Invoke-WebRequest -uri $dpath -OutFile $outpath;
+    Start-Sleep -Seconds 360
+}
